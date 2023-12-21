cd4f1050524a0f344eed0653381b5995abd76cd4
Someone772 <92219638+Someone772@users.noreply.github.com>
2023-12-21 11:35:58 -0300

    Update anykernel.sh
    dwh

--- anykernel.sh
+++ anykernel.sh
@@ -10,11 +10,11 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=jeter
-device.name2=aljeter
-device.name3=jeter
-device.name4=alijeter
-device.name5=jeter
+device.name1=evert
+device.name2=evert
+device.name3=evert
+device.name4=evert
+device.name5=evert
 supported.versions=
 supported.patchlevels=
 '; } # end properties
